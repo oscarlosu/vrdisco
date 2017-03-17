813a1e4df918bea2371a29c65ba8441d78ef8972
Phillip Phoelich <pphoelich@gmail.com>
2017-03-17 22:19:09 +0100

    Some music system stuff...

---
+++ vrdisco/Assets/Scripts/AudioSystem/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MusicSystem;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public static MusicPlayer Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+}
