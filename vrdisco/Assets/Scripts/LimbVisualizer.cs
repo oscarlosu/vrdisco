f962dc7f013db9644706b8f836d4f7e8981af670
Oscar Losada <oscarlosu@gmail.com>
2017-03-18 16:20:13 +0100

    Apple avatar with limb ik arms and legs.

---
+++ vrdisco/Assets/Scripts/LimbVisualizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LimbVisualizer : MonoBehaviour {
+    [SerializeField]
+    protected List<Transform> _joints;
+
+    private LineRenderer _line;
+
+    void Start () {
+        _line = GetComponent<LineRenderer>();
+        _line.numPositions = _joints.Count;
+	}
+
+    void FixedUpdate () {
+        for(int i = 0; i < _joints.Count; ++i) {
+            _line.SetPosition(i, _joints[i].position);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        for (int i = 0; i < _joints.Count - 1; ++i) {
+            Gizmos.DrawLine(_joints[i].position, _joints[i+1].position);
+        }
+    }
+}
